ff34d3a693eb733f0879c1f9bf8cde170b78d1b4
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2024-06-22 00:37:24 +1000

    added content of astra_legacy_docker into this

---
+++ docker/workspace_entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# start udev
+/lib/systemd/systemd-udevd --daemon
+
+# setup ros2 environment
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+source "$WORKSPACE_ROOT/install/setup.bash"
+
+exec "$@"
